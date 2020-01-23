066baa23c8972103db826a0c9440ef35c977eb85
technothinksup <project.technothinksup@gmail.com>
2020-01-23 17:55:35 +0530

    active member design

--- application/views/Website/active_members.php
+++ application/views/Website/active_members.php
@@ -30,21 +30,19 @@
 
             <div class="tab-div">
               <div class="row">
-                <table class="table">
+                <table class="table no-padding no-top-border">
                   <tbody>
                     <tr>
-                      <td width="25%"><div class="profile-img">
+                      <td width="25%"><div class="profile-img no-top-border">
                         <img class="active-mem-img" src="<?php echo base_url(); ?>assets/images/profile-girl.jpg" width="100%" alt="">
                       </div></td>
-                      <td>
+                      <td class="no-padding no-top-border">
                         <table class="table  active-table">
                           <tbody>
                             <tr>
-                               <th scope="row" style="font-size:18px;"  class="text-danger" colspan="4">Rahul Nikam</th>
-                            </tr>
-                            <tr>
-                              <th>Member ID</th>
-                               <td class="text-danger" colspan="3">	3161CC63124</td>
+                               <th scope="row"  class="text-danger text-center font-16" colspan="2">Rahul Nikam</th>
+                               <th>Member ID</th>
+                                <td class="text-danger">	3161CC63124</td>
                             </tr>
                             <tr>
                               <th >Age</th>
@@ -64,25 +62,23 @@
                               <th>Marital Status</th>
                               <td>Unmarride</td>
                             </tr>
+                            <tr>
+                              <td colspan="4 ">
+                                <ul  class="inline" style="display: inline; list-style-type:none;">
+                                <li class="pt-2">
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-address-card" aria-hidden="true"></i> Full Profile</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Express Interest</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Send Massege</button>
+                                </li>
+                              </ul>
+                            </td>
+                            </tr>
                           </tbody>
                         </table>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colspan="2">
-                        <div class="col-md-12">
-                          <ul  class="inline" style="display: inline; list-style-type:none;">
-                            <li>
-                              <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-address-card" aria-hidden="true"></i> Full Profile</button>
-                            </li>
-                            <li>
-                              <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Express Interest</button>
-                            </li>
-                            <li>
-                              <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Send Massege</button>
-                            </li>
-                          </ul>
-                        </div>
                       </td>
                     </tr>
                   </tbody>
@@ -90,6 +86,181 @@
               </div>
             </div>
 
+
+            <div class="tab-div">
+              <div class="row">
+                <table class="table no-padding no-top-border">
+                  <tbody>
+                    <tr>
+                      <td width="25%"><div class="profile-img no-top-border">
+                        <img class="active-mem-img" src="<?php echo base_url(); ?>assets/images/profile-girl.jpg" width="100%" alt="">
+                      </div></td>
+                      <td class="no-padding no-top-border">
+                        <table class="table  active-table">
+                          <tbody>
+                            <tr>
+                               <th scope="row"  class="text-danger text-center font-16" colspan="2">Rahul Nikam</th>
+                               <th>Member ID</th>
+                                <td class="text-danger">	3161CC63124</td>
+                            </tr>
+                            <tr>
+                              <th >Age</th>
+                              <td>34</td>
+                              <th> Height</th>
+                              <td>7.00 Feet</td>
+                            </tr>
+                            <tr>
+                              <th >Religion</th>
+                              <td>Hindu</td>
+                            <th> Caste / Sect</th>
+                              <td>Maratha</td>
+                            </tr>
+                            <tr>
+                              <th>Mother Tongue</th>
+                              <td>Marathi</td>
+                              <th>Marital Status</th>
+                              <td>Unmarride</td>
+                            </tr>
+                            <tr>
+                              <td colspan="4 ">
+                                <ul  class="inline" style="display: inline; list-style-type:none;">
+                                <li class="pt-2">
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-address-card" aria-hidden="true"></i> Full Profile</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Express Interest</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Send Massege</button>
+                                </li>
+                              </ul>
+                            </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <div class="tab-div">
+              <div class="row">
+                <table class="table no-padding no-top-border">
+                  <tbody>
+                    <tr>
+                      <td width="25%"><div class="profile-img no-top-border">
+                        <img class="active-mem-img" src="<?php echo base_url(); ?>assets/images/profile-girl.jpg" width="100%" alt="">
+                      </div></td>
+                      <td class="no-padding no-top-border">
+                        <table class="table  active-table">
+                          <tbody>
+                            <tr>
+                               <th scope="row"  class="text-danger text-center font-16" colspan="2">Rahul Nikam</th>
+                               <th>Member ID</th>
+                                <td class="text-danger">	3161CC63124</td>
+                            </tr>
+                            <tr>
+                              <th >Age</th>
+                              <td>34</td>
+                              <th> Height</th>
+                              <td>7.00 Feet</td>
+                            </tr>
+                            <tr>
+                              <th >Religion</th>
+                              <td>Hindu</td>
+                            <th> Caste / Sect</th>
+                              <td>Maratha</td>
+                            </tr>
+                            <tr>
+                              <th>Mother Tongue</th>
+                              <td>Marathi</td>
+                              <th>Marital Status</th>
+                              <td>Unmarride</td>
+                            </tr>
+                            <tr>
+                              <td colspan="4 ">
+                                <ul  class="inline" style="display: inline; list-style-type:none;">
+                                <li class="pt-2">
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-address-card" aria-hidden="true"></i> Full Profile</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Express Interest</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Send Massege</button>
+                                </li>
+                              </ul>
+                            </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+
+            <div class="tab-div">
+              <div class="row">
+                <table class="table no-padding no-top-border">
+                  <tbody>
+                    <tr>
+                      <td width="25%"><div class="profile-img no-top-border">
+                        <img class="active-mem-img" src="<?php echo base_url(); ?>assets/images/profile-girl.jpg" width="100%" alt="">
+                      </div></td>
+                      <td class="no-padding no-top-border">
+                        <table class="table  active-table">
+                          <tbody>
+                            <tr>
+                               <th scope="row"  class="text-danger text-center font-16" colspan="2">Rahul Nikam</th>
+                               <th>Member ID</th>
+                                <td class="text-danger">	3161CC63124</td>
+                            </tr>
+                            <tr>
+                              <th >Age</th>
+                              <td>34</td>
+                              <th> Height</th>
+                              <td>7.00 Feet</td>
+                            </tr>
+                            <tr>
+                              <th >Religion</th>
+                              <td>Hindu</td>
+                            <th> Caste / Sect</th>
+                              <td>Maratha</td>
+                            </tr>
+                            <tr>
+                              <th>Mother Tongue</th>
+                              <td>Marathi</td>
+                              <th>Marital Status</th>
+                              <td>Unmarride</td>
+                            </tr>
+                            <tr>
+                              <td colspan="4 ">
+                                <ul  class="inline" style="display: inline; list-style-type:none;">
+                                <li class="pt-2">
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-address-card" aria-hidden="true"></i> Full Profile</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Express Interest</button>
+                                </li>
+                                <li>
+                                  <button style="width:120px;" class="btn btn-success btn-sm" type="submit"><i class="fa fa-heart" aria-hidden="true"></i> Send Massege</button>
+                                </li>
+                              </ul>
+                            </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
 
             <br><br>
           </div>
